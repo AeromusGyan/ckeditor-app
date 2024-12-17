547eb7e49d24d60a050b3f11f8d94982b7cafe11
AeromusGyan <aeromusgyan@gmail.com>
2024-12-18 01:00:41 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AngularEditor from 'ckeditor5-custom-build/build/ckeditor';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ckeditor-app';
+
+export class AppComponent implements OnInit {
+	public watchdog: any;
+	public ready = false;
+	public htmlData:string = "<p>Hello, world!</p>"
+
+	public Editor: any = AngularEditor;
+
+	public isDisabled = false;
+
+	public config = {
+		
+	}
+
+	public onReady(editor: any): void {
+		console.log(editor);
+	}
+
+	public ngOnInit(): void {
+		const contextConfig: any = {
+			foo: 'bar'
+		};
+
+		// this.config = {
+		// 	collaboration: {
+		// 		channelId: 'foobar-baz'
+		// 	}
+		// };
+
+		// this.watchdog = new ContextWatchdog( );
+
+		// this.watchdog.create( contextConfig )
+		// 	.then( () => {
+		// 		this.ready = true;
+		// 	} );
+	}
+
+	public toggle(): void {
+		this.isDisabled = !this.isDisabled;
+	}
+
+
 }
